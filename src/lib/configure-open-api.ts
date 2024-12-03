e24621feb86a1af0ee17aa685eac8592b4581a00
laszlovit <laszlo.vitkai@gmail.com>
2024-12-03 14:57:59 +0100

    implemented scalar and setup config like theme, layout etc...

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,11 +1,27 @@
+import { apiReference } from "@scalar/hono-api-reference";
+
 import type { AppOpenAPI } from "./types";
+
+import packageJson from "../../package.json";
 
 export default function ConfigureOpenApi(app: AppOpenAPI) {
   app.doc("/doc", {
     openapi: "3.0.0",
     info: {
       title: "De Blå Helte API",
-      version: "1.0.0",
+      version: packageJson.version,
     },
   });
+
+  app.get("reference", apiReference({
+    theme: "kepler",
+    layout: "classic",
+    defaultHttpClient: {
+      targetKey: "javascript",
+      clientKey: "fetch",
+    },
+    spec: {
+      url: "/doc",
+    },
+  }));
 }
