6f2a7bb2bc00b968382ddc3b65c2399c58cb744c
laszlovit <laszlo.vitkai@gmail.com>
2024-12-03 10:33:11 +0100

    added pino-logger and setup type safe env implementation

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
+import type { PinoLogger } from "hono-pino";
+
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { notFound, onError } from "stoker/middlewares";
 
-const app = new OpenAPIHono();
+import { pinoLogger } from "./middlewares/pino-logger";
+
+interface AppBindigs {
+  Variables: {
+    logger: PinoLogger;
+  };
+}
+
+const app = new OpenAPIHono<AppBindigs>();
+app.use(pinoLogger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
 app.get("/error", (c) => {
-  c.status(422);
+  c.var.logger.info("This is an info log");
   throw new Error("This is an error");
 });
 
